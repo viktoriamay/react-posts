5fcb4ecad8305e3384e5eb547b2d6b46a22b4032
viktoriamay <alterkelly@mail.ru>
2023-03-20 02:55:41 +0300

    Add header icons menu. Refactor styles in header menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { HeartOutlined, PlusCircleOutlined, SmileOutlined } from '@ant-design/icons';
 import { Logo } from '../Logo/Logo';
 import { Search } from '../Search/Search';
 import './Header.scss';
@@ -6,11 +7,18 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="container">
-      <div className='header__wrapper'>
-        <Logo />
-        <Search />
-      </div>
+        <div className="header__wrapper">
+          <div className="header__action_menu">
+            <Logo />
+            <Search />
+          </div>
+          <div className="header__icons_menu">
+            <PlusCircleOutlined className="header__add_icon" />
+            <HeartOutlined className="header__favorites_icon" />
+            <SmileOutlined className="header__login_icon" />
+          </div>
+        </div>
       </div>
     </header>
-  )
-}
+  );
+};
