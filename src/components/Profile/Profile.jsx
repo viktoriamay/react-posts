5ef55263274368518eb9fc6e391b1ada99b2c3de
viktoriamay <alterkelly@mail.ru>
2023-03-30 04:36:05 +0300

    Refactor Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -6,11 +6,11 @@ import { VALIDATE_CONFIG } from './../../constants/constants';
 import api from './../../utils/api';
 import { useNavigate } from 'react-router-dom';
 import { LeftOutlined } from '@ant-design/icons';
-import './Profile.scss'
+import './Profile.scss';
 
 export const Profile = () => {
-  const { currentUser, setCurrentUser } = useContext(PostsContext);
-  // console.log({ currentUser });
+  const { currentUser, editProfileRequest, editAvatarRequest, handleLogout } =
+    useContext(PostsContext);
 
   const {
     register,
@@ -18,40 +18,30 @@ export const Profile = () => {
     formState: { errors },
   } = useForm({ mode: 'onBlur' });
 
-  const editProfile =  (data) => {
-    api.editUserInfo({name: data.name, about: data.about}).then((newUser) => setCurrentUser({...newUser}));
-  };
-
-  const editAvatar =  (src) => {
-     api.editUserAvatar({avatar: src.avatar}).then((newUser) => setCurrentUser({...newUser}));
-  };
-
   const required = {
     value: true,
     message: VALIDATE_CONFIG.requiredMessage,
   };
 
   const navigate = useNavigate();
-  const handleLogout = () => {
-    localStorage.removeItem('token');
-    navigate('/');
-  };
 
   return (
     <div>
       <div onClick={() => navigate(-1)} className="post__button_back">
-          <LeftOutlined />
-          <span>Назад</span>
-        </div>
+        <LeftOutlined />
+        <span>Назад</span>
+      </div>
       <h2>Редактирование профиля</h2>
       {currentUser ? (
-        <div className='profile'>
-        <div className='profile__avatar'>
-
-        <img src={currentUser.avatar} />
-        </div>
-        <Form className="profile__form" handleFormSubmit={handleSubmit(editAvatar)}>
-            <input className='form__input'
+        <div className="profile">
+          <div className="profile__avatar">
+            <img src={currentUser.avatar} alt='user-avatar' />
+          </div>
+          <Form
+            className="profile__form"
+            handleFormSubmit={handleSubmit(editAvatarRequest)}>
+            <input
+              className="form__input"
               {...register('avatar', {
                 required,
               })}
@@ -61,10 +51,15 @@ export const Profile = () => {
               defaultValue={currentUser.avatar}
             />
             {errors.name && <p>{errors?.about.message}</p>}
-            <button className='form__button' type='submit'>Изменить аватар</button>
+            <button className="form__button" type="submit">
+              Изменить аватар
+            </button>
           </Form>
-          <Form className="profile__form" handleFormSubmit={handleSubmit(editProfile)}>
-            <input className='form__input'
+          <Form
+            className="profile__form"
+            handleFormSubmit={handleSubmit(editProfileRequest)}>
+            <input
+              className="form__input"
               {...register('name', {
                 required,
               })}
@@ -74,7 +69,8 @@ export const Profile = () => {
               defaultValue={currentUser.name}
             />
             {errors.name && <p>{errors?.name.message}</p>}
-            <input className='form__input'
+            <input
+              className="form__input"
               {...register('about', {
                 required,
               })}
@@ -85,7 +81,8 @@ export const Profile = () => {
             />
             {errors.name && <p>{errors?.about.message}</p>}
 
-            <input className='form__input'
+            <input
+              className="form__input"
               type="email"
               name="email"
               placeholder="email"
@@ -93,20 +90,16 @@ export const Profile = () => {
               disabled
             />
 
-            {/* <input className='form__input'
-              type="text"
-              name="id"
-              placeholder="id"
-              defaultValue={currentUser._id}
-              disabled
-            /> */}
-
-            <button className='form__button' type="submit">Изменить данные профиля</button>
-          </Form>
-            <button className='form__button form__button_profile' type="button" onClick={handleLogout}>
-              Выход
+            <button className="form__button" type="submit">
+              Изменить данные профиля
             </button>
-          
+          </Form>
+          <button
+            className="form__button form__button_profile"
+            type="button"
+            onClick={handleLogout}>
+            Выход
+          </button>
         </div>
       ) : (
         <div>Error</div>
