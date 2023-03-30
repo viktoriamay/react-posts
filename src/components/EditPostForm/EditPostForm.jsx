dbe3aada134da9783669dc1ed7f0a52d1cb44654
viktoriamay <alterkelly@mail.ru>
2023-03-30 15:07:55 +0300

    Refactor EditPostForm.jsx

--- src/components/EditPostForm/EditPostForm.jsx
+++ src/components/EditPostForm/EditPostForm.jsx
@@ -1,76 +1,30 @@
 import { Form } from '../Form/Form';
 import { useForm } from 'react-hook-form';
 import { VALIDATE_CONFIG } from '../../constants/constants';
-import { useContext, useEffect, useState } from 'react';
-import { PostsContext } from '../../context/PostsContext';
-import { Modal } from '../Modal/Modal';
+import { useState } from 'react';
 import './EditPostForm.scss';
 
-export const EditPostForm = ({ editPost, post }) => {
+export const EditPostForm = ({ editPostRequest, post, headerCloseModal }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm({ mode: 'onBlur' });
 
-  const { posts, setPosts, addPost, setActiveModal, activeModal } =
-    useContext(PostsContext);
-
   const required = {
     value: true,
     message: VALIDATE_CONFIG.requiredMessage,
   };
 
-  /* const onSendComment = (data) => {
-    // setIsLoading(true);
-    api
-      .addComment(post._id, data)
-      .then((result) => {
-        setPost({ ...result });
-        // openNotification('success', 'Успешно', 'Ваш отзыв успешно отправлен');
-      })
- 
-  }; */
+  const [imageSrc, setImageSrc] = useState(post?.image); // изображение из инпута
 
-  /*  const handlePostLike = (post) => {
-    // найти в посте в массиве лайков тот айди, который будет равен каррентЮзер._айди
-
-    // другая запись функции выше без дополнительных переменных:
-    // const liked = post.likes.some((id) => id === currentUser?._id);
-
-    api.changeLikePost(post?._id, liked).then((newCard) => {
-      const newPosts = posts?.map((postState) => {
-        // console.log('Карточка из стейта', postState);
-        // console.log('Карточка с сервера', newCard);
-        return postState?._id === newCard?._id ? newCard : postState;
-      });
-      
-
-        // отфильтруй так, чтобы кард айди не содержал новый кард айди
-        setFavorites((prevState) =>
-          prevState.filter((card) => card._id !== newCard._id),
-        );
-      }
-
-      // в конце показываем карточки (сетим посты, отрисовываем) с учетом изменений (то есть изменяем те посты в которых что то поменялось)
-      setPosts(newPosts);
-    });
-  }; */
-
-   const [imageSrc, setImageSrc] = useState(post?.image);
-
-  const handleImageChange = (e) => {
-    setImageSrc(e.target.value);
+  function handleImageChange(event) {
+    setImageSrc(event.target.value); // достаем изображение вводимое в инпут
   }
-/*
-  const [activeHeaderModal, setActiveHeaderModal] = useState({
-    isOpen: false,
-    component: 'register',
-  }); */
 
   return (
     <Form
-      handleFormSubmit={handleSubmit(editPost)}
+      handleFormSubmit={handleSubmit(editPostRequest)}
       title="Изменить пост"
       className="edit_post_form">
       <input
