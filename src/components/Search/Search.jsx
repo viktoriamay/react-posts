53573bc7f190e783307d56d4159fc92cf4dffd7d
viktoriamay <alterkelly@mail.ru>
2023-03-30 04:40:26 +0300

    Refactor Search.jsx

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,26 +1,29 @@
 import { SearchOutlined } from '@ant-design/icons';
-import './Search.scss'
+import './Search.scss';
+import { useContext } from 'react';
+import { PostsContext } from './../../context/PostsContext';
 
-export const Search = ({onSubmit: formSubmitRequest, onInput: changeInput, isAuth}) => {
+export const Search = () => {
+  const { isAuth, formSubmitRequest, changeInput } = useContext(PostsContext);
 
   // на форму вешаем функцию из апы фильтрации постов formSubmitRequest по нажатию на сабмит
   // на инпут вешаем функцию из апы changeInput, которая вытаскивает вводимое в инпут значение
 
-
   return (
-<> {isAuth &&
-
-    <form className="search" onSubmit={formSubmitRequest}>
-      <input 
-        type="text"
-        className="search__input"
-        placeholder="Поиск"
-        onInput={changeInput} />
-      <button className="search__btn">
-      <SearchOutlined />
-      </button>
-    </form>
-}
-</>
-  )
-}
+    <>
+      {isAuth && (
+        <form className="search" onSubmit={formSubmitRequest}>
+          <input
+            type="text"
+            className="search__input"
+            placeholder="Поиск"
+            onInput={changeInput}
+          />
+          <button className="search__btn">
+            <SearchOutlined />
+          </button>
+        </form>
+      )}
+    </>
+  );
+};
