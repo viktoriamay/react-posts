6b7a3e52f9924afdfa2e3ad042fd62f10619d424
viktoriamay <alterkelly@mail.ru>
2023-04-01 20:50:43 +0300

    Add update posts state after add / delete comment

--- src/pages/PostPage/PostPage.jsx
+++ src/pages/PostPage/PostPage.jsx
@@ -5,25 +5,25 @@ import { useParams } from 'react-router-dom';
 import { PostsContext } from './../../context/PostsContext';
 import api from './../../utils/api';
 
-export const PostPage = ({userById}) => {
+export const PostPage = ({ userById }) => {
   const [postCurrentUser, setPostCurrentUser] = useState(null); // текущий юзер на странице поста
   const [post, setPost] = useState(null); // текущий пост
   const [isLoading, setIsLoading] = useState(false);
   const isLike = post?.likes?.some((id) => id === postCurrentUser?._id);
   const [isClicked, setClicked] = useState(isLike);
 
-
   const {
     favorites,
     setPosts,
+    posts,
     activeModal,
     setActiveModal,
     handlePostLike,
-    users, 
-     activeHeaderModal, setActiveHeaderModal, getUserCommentsInfo
+    users,
+    activeHeaderModal,
+    setActiveHeaderModal,
+    getUserCommentsInfo,
   } = useContext(PostsContext);
-
-  
 
   // парамс это то, что приходит в апе в роутах path="/post/:postId", а именно :postId - динамический путь это и есть парамс
   const params = useParams();
@@ -34,7 +34,7 @@ export const PostPage = ({userById}) => {
 
   useEffect(() => {
     setIsLoading(true);
-    
+
     Promise.all([api.getUserInfo(), api.getPostById(params.postId)])
       .then(([userData, postData]) => {
         setPostCurrentUser(userData);
@@ -74,9 +74,21 @@ export const PostPage = ({userById}) => {
     api
       .addComment(post._id, data)
       .then((result) => {
-        setPost({ ...result });
+        const updatedPost = { ...result };
+        // console.log(...result);
+        setPost(updatedPost);
         // openNotification('success', 'Успешно', 'Ваш отзыв успешно отправлен');
+        return updatedPost; // возвращаем обновленный пост из метода then
       })
+      .then((updatedPost) => {
+        const newPosts = posts?.map((postsState) => {
+          // получаем массив новых карточек после постановки лайка и возвращаем старые карточки, если лайк не поставлен и новую если поставлен
+          return postsState?._id === updatedPost?._id ? updatedPost : postsState;
+        });
+
+        setPosts(newPosts); // передаем новое состояние posts в setPosts
+      })
+
       .catch((error) => {
         // openNotification('error', 'Ошибка', 'Не получилось отправить отзыв');
       })
@@ -89,18 +101,21 @@ export const PostPage = ({userById}) => {
     api
       .deleteComment(post._id, comment)
       .then((result) => {
-        setPost({ ...result });
+        const updatedPost = { ...result };
+        setPost(updatedPost);
         // openNotification('success', 'Успешно', 'Ваш отзыв успешно отправлен');
+        return updatedPost;
+      })
+      .then((updatedPost) => {
+        const updatedPosts = posts.map((postsState) => {
+          return postsState._id === updatedPost._id ? updatedPost : postsState;
+        });
+        setPosts(updatedPosts);
       })
       .catch((error) => {
         // openNotification('error', 'Ошибка', 'Не получилось отправить отзыв');
       });
   };
-
-
-
-  console.log({userById});
-
 
   return (
     <div>
@@ -118,13 +133,11 @@ export const PostPage = ({userById}) => {
           editPostRequest={editPostRequest}
           users={users}
           isLike={isLike}
-          
           activeHeaderModal={activeHeaderModal}
           setActiveHeaderModal={setActiveHeaderModal}
           activeModal={activeModal}
           setActiveModal={setActiveModal}
           postCloseModal={postCloseModal}
-
           getUserCommentsInfo={getUserCommentsInfo}
         />
       )}
