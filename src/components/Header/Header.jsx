74853917309ec993698dade1adef8c633d14eaf0
viktoriamay <alterkelly@mail.ru>
2023-04-02 04:28:40 +0300

    Add hide add post icon when !isAuth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,7 +14,13 @@ import { Modal } from '../Modal/Modal';
 import { AddPostForm } from './../AddPostForm/AddPostForm';
 
 export const Header = ({ children }) => {
-  const { favorites, isAuth, setIsAuth, activeHeaderModal, setActiveHeaderModal } = useContext(PostsContext);
+  const {
+    favorites,
+    isAuth,
+    setIsAuth,
+    activeHeaderModal,
+    setActiveHeaderModal,
+  } = useContext(PostsContext);
 
   const navigate = useNavigate();
 
@@ -33,23 +39,25 @@ export const Header = ({ children }) => {
             {children}
           </div>
           <div className="header__icons_menu">
-            <PlusCircleOutlined
-              onClick={() =>
-                setActiveHeaderModal({ component: 'addPost', isOpen: true })
-              }
-              className="header__add_icon"
-            />
             {isAuth && (
-              <Link to={'/favorites'}>
-                <div className="header__favorites_icon_wrapper">
-                  {favorites.length !== 0 && (
-                    <span className="header__favorites_counter">
-                      {favorites.length}
-                    </span>
-                  )}
-                  <HeartOutlined className="header__favorites_icon" />
-                </div>
-              </Link>
+              <>
+                <PlusCircleOutlined
+                  onClick={() =>
+                    setActiveHeaderModal({ component: 'addPost', isOpen: true })
+                  }
+                  className="header__add_icon"
+                />
+                <Link to={'/favorites'}>
+                  <div className="header__favorites_icon_wrapper">
+                    {favorites.length !== 0 && (
+                      <span className="header__favorites_counter">
+                        {favorites.length}
+                      </span>
+                    )}
+                    <HeartOutlined className="header__favorites_icon" />
+                  </div>
+                </Link>
+              </>
             )}
             {isAuth ? (
               <SmileOutlined
