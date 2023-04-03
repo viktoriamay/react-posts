c9d592efa6f241e1656c9f6db4c754907978ea09
viktoriamay <alterkelly@mail.ru>
2023-04-03 23:19:12 +0300

    Add UserProfile page

--- src/components/UsersProfile/UsersProfile.jsx
+++ src/components/UsersProfile/UsersProfile.jsx
@@ -5,6 +5,8 @@ import { PostPage } from '../../pages/PostPage/PostPage';
 import { Post } from './../Post/Post';
 import { PostsContext } from './../../context/PostsContext';
 import { PagesBackButton } from '../PagesBackButton/PagesBackButton';
+import './UsersProfile.scss';
+import { NumberOutlined, SendOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
 
 export const UsersProfile = () => {
   const [userById, setUserById] = useState(null);
@@ -22,9 +24,28 @@ export const UsersProfile = () => {
       <div className="button_back_offset">
         <PagesBackButton />
       </div>
-      <img src={userById?.avatar} />
-      <h2>{userById?.name}</h2>
-      <p>{userById?.about}</p>
+      <div className="user_profile">
+        <div className="user_profile__avatar">
+          <img src={userById?.avatar} alt="user-avatar" />
+        </div>
+        <h2 className="user_profile__name">{userById?.name}</h2>
+        <div className="user_profile__info">
+        <UserOutlined />
+          <p >{userById?.about}</p>
+        </div>
+        <div className="user_profile__info">
+        <SendOutlined />
+          <p>{userById?.email}</p>
+        </div>
+        <div className="user_profile__info">
+        <NumberOutlined />
+          <p>{userById?._id}</p>
+        </div>
+        <div className="user_profile__info">
+        <TeamOutlined />
+          <p>{userById?.group}</p>
+        </div>
+      </div>
     </div>
   );
 };
