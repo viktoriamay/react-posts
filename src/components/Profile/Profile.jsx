dc22414edc9e5ca74ff57a74ecb35446c0c45709
viktoriamay <alterkelly@mail.ru>
2023-04-06 01:28:25 +0300

    Edit Profile

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,10 +3,10 @@ import { PostsContext } from './../../context/PostsContext';
 import { useForm } from 'react-hook-form';
 import { Form } from './../Form/Form';
 import { VALIDATE_CONFIG } from './../../constants/constants';
-import { useNavigate } from 'react-router-dom';
-import { LeftOutlined } from '@ant-design/icons';
+import { LogoutOutlined } from '@ant-design/icons';
 import './Profile.scss';
 import { PagesBackButton } from '../PagesBackButton/PagesBackButton';
+import { Spinner } from './../Spinner/Spinner';
 
 export const Profile = () => {
   const { currentUser, editProfileRequest, editAvatarRequest, handleLogout } =
@@ -16,69 +16,86 @@ export const Profile = () => {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm({ mode: 'onBlur' });
+  } = useForm({ mode: 'onChange' });
 
   const required = {
     value: true,
     message: VALIDATE_CONFIG.requiredMessage,
   };
 
-  const navigate = useNavigate();
-
   return (
     <div>
-      <div className='button_back_offset'>
+      <div className="button_back_offset">
         <PagesBackButton />
+        <LogoutOutlined onClick={handleLogout} className="header__login_icon" />
       </div>
       <h2>Редактирование профиля</h2>
       {currentUser ? (
         <div className="profile">
           <div className="profile__avatar">
-            <img src={currentUser.avatar} alt="user-avatar" />
+            <img src={currentUser?.avatar} alt="user-avatar" />
           </div>
           <Form
             className="profile__form"
             handleFormSubmit={handleSubmit(editAvatarRequest)}>
-            <input
-              className="form__input"
-              {...register('avatar', {
-                required,
-              })}
-              type="text"
-              name="avatar"
-              placeholder="avatar"
-              defaultValue={currentUser.avatar}
-            />
-            {errors.name && <p>{errors?.about.message}</p>}
-            <button className="form__button" type="submit">
+            <div className="profile__input_block">
+              <input
+                className="form__input"
+                {...register('avatar', {
+                  required,
+                })}
+                type="text"
+                name="avatar"
+                placeholder="Avatar"
+                defaultValue={currentUser?.avatar}
+              />
+              {errors.avatar && (
+                <span className="form__errors profile_form__errors">
+                  {errors?.avatar.message}
+                </span>
+              )}
+            </div>
+            <button className="form__button profile__button" type="submit">
               Изменить аватар
             </button>
           </Form>
           <Form
-            className="profile__form"
+            className="profile__form form__form"
             handleFormSubmit={handleSubmit(editProfileRequest)}>
-            <input
-              className="form__input"
-              {...register('name', {
-                required,
-              })}
-              type="text"
-              name="name"
-              placeholder="Имя"
-              defaultValue={currentUser.name}
-            />
-            {errors.name && <p>{errors?.name.message}</p>}
-            <input
-              className="form__input"
-              {...register('about', {
-                required,
-              })}
-              type="text"
-              name="about"
-              placeholder="Обо мне"
-              defaultValue={currentUser.about}
-            />
-            {errors.name && <p>{errors?.about.message}</p>}
+            <div className="profile__input_block">
+              <input
+                className="form__input"
+                {...register('name', {
+                  required,
+                })}
+                type="text"
+                name="name"
+                placeholder="Имя"
+                defaultValue={currentUser.name}
+              />
+              {errors.name && (
+                <span className="form__errors profile_form__errors">
+                  {errors?.name.message}
+                </span>
+              )}
+            </div>
+            <div className="profile__input_block">
+              <input
+                className="form__input"
+                {...register('about', {
+                  required,
+                })}
+                type="text"
+                name="about"
+                placeholder="Обо мне"
+                defaultValue={currentUser.about}
+              />
+              {errors.about && (
+                <span className="form__errors profile_form__errors">
+                  {errors?.about.message}
+                </span>
+              )}
+            </div>
 
             <input
               className="form__input"
@@ -89,19 +106,13 @@ export const Profile = () => {
               disabled
             />
 
-            <button className="form__button" type="submit">
+            <button className="form__button profile__button" type="submit">
               Изменить данные профиля
             </button>
           </Form>
-          <button
-            className="form__button form__button_profile"
-            type="button"
-            onClick={handleLogout}>
-            Выход
-          </button>
         </div>
       ) : (
-        <div>Error</div>
+        <Spinner />
       )}
     </div>
   );
