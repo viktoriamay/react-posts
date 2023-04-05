34f41f9307b9542768c5bfa40ab687107491fbb0
viktoriamay <alterkelly@mail.ru>
2023-04-05 17:08:46 +0300

    Add pagination

--- src/pages/PostsPage/PostsPage.jsx
+++ src/pages/PostsPage/PostsPage.jsx
@@ -1,19 +1,63 @@
-import { CardList } from '../../components/CardList/CardList';
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { PostsContext } from '../../context/PostsContext';
 import { Sort } from '../../components/Sort/Sort';
+import { Card } from '../../components/Card/Card';
+import { Pagination } from 'antd';
+import './PostsPage.scss';
 
 export const PostsPage = () => {
-  const { posts, isAuth } = useContext(PostsContext);
+  const { posts } = useContext(PostsContext);
+
+  const [paginated, setPaginated] = useState([]);
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(6);
+
+  const locale = {
+    items_per_page: 'постов на странице',
+    jump_to: 'Перейти',
+    page: '',
+    prev_page: 'Предыдущая страница',
+    next_page: 'Следующая страница',
+    prev_5: 'Предыдущие 5 страниц',
+    next_5: 'Следующие 5 страниц',
+    prev_3: 'Предыдущие 3 страницы',
+    next_3: 'Следующие 3 страницы',
+  };
+
+  useEffect(() => {
+    const sliced = posts.slice((page - 1) * size, size * page);
+    setPaginated(sliced);
+  }, [page, posts, size]);
+
+  useEffect(() => {
+    setPage(1);
+  }, [posts.length, size]);
 
   return (
-    <>
-      {isAuth && (
-        <div>
-          {posts?.length > 1 && <Sort />}
-          <CardList posts={posts} />
-        </div>
+    <div>
+      {posts?.length > 1 && <Sort />}
+      <div className="cards">
+        {paginated.map((post) => (
+          <Card key={post?._id + Math.random()} {...post} post={post} />
+        ))}
+      </div>
+      {posts?.length > 6 && (
+        <Pagination
+          locale={locale}
+          defaultCurrent={1}
+          current={page}
+          total={Math.ceil(posts.length)}
+          showSizeChanger
+          defaultPageSize={6}
+          pageSizeOptions={[6, 9, 30]}
+          className="pagination"
+          pageSize={size}
+          onChange={(page, size) => {
+            setPage(page);
+            setSize(size);
+          }}
+        />
       )}
-    </>
+    </div>
   );
 };
