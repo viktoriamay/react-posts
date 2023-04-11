15486f78a9eca7bbdcacbd05a900819353f8fa0a
viktoriamay <alterkelly@mail.ru>
2023-04-12 02:43:35 +0300

    Add adaptive to Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,11 +1,11 @@
 import { ReactComponent as MainLogo } from './img/main-logo.svg';
 import './Logo.scss'
 
-export const Logo = () => {
+export const Logo = ({logoTitle}) => {
   return (
     <div className='logo'>
       <MainLogo className='logo__img' />
-      <div className='logo__title_wrapper'>
+      <div className={ logoTitle ? 'logo__title_wrapper' : 'title__footer'}>
         <p className='logo__title'>React</p>
         <p className='logo__title'>Posts</p>
       </div>
