a97aeb456ba784879f016555438acecf980b814b
viktoriamay <alterkelly@mail.ru>
2023-04-12 02:41:04 +0300

    Add NotFound page

---
+++ src/pages/NotFound/NotFound.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import './NotFound.scss';
+import { FrownOutlined } from '@ant-design/icons';
+
+export const NotFound = () => {
+  return (
+    <div className="not_found">
+      <h2 className="not_found__title">
+        Страница не найдена <FrownOutlined />
+      </h2>
+      <Link to="/" className="not_found__link">
+        Обратно на главную
+      </Link>
+    </div>
+  );
+};
