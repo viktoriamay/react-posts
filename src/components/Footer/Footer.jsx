f81054706c9a02e2b63838eb3a6a166f4fad9f96
viktoriamay <alterkelly@mail.ru>
2023-04-12 02:42:23 +0300

    Add date to footer copyright and adaptive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,16 +12,19 @@ export const Footer = () => {
     <div className="footer">
       <div className="container">
         <div className="footer__wrapper">
-          <p>Made by viktoriamay</p>
+          <p className="footer__madeby">
+            <span>Made by viktoriamay</span>
+            <span>© {new Date().getFullYear()}</span>
+          </p>
           <div className="footer__socials">
             <a href="https://github.com/viktoriamay" target="_blank">
-              <GithubOutlined className="header__login_icon" />
+              <GithubOutlined className="project_icon__svg" />
             </a>
-            <a href="mailto:alterkelly@mail.ru" target="_blank">
-              <MessageOutlined className="header__login_icon" />
+            <a href="mailto:viktoriagmay@gmail.com" target="_blank">
+              <MessageOutlined className="project_icon__svg" />
             </a>
             <a href="https://t.me/etern8ty" target="_blank">
-              <SendOutlined className="header__login_icon" />
+              <SendOutlined className="project_icon__svg" />
             </a>
           </div>
           <Link to={'/'}>
